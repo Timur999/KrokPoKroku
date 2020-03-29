ba8ba434fd3785736231c477772d85c00aff24aa
dota2 <dota2@poczta.onet.pl>
2020-03-29 23:36:52 +0200

    init

---
+++ methods/methods/CalculateSalary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods
+{
+    class CalculateSalary
+    {
+        public static void Start()
+        {
+            (new CalculateSalary()).run();
+        }
+        void run()
+        {
+            double dailyRate = ReadDouble("Enter your daily rate:");
+            int noOfDays = ReadInt("Enter the numbers of days:");
+            WriteFee(CalculateFee(dailyRate, noOfDays));
+        }
+
+        private void WriteFee(double v) => Console.WriteLine($"Your fee is: {v * 0.9}");
+
+        private double CalculateFee(double dailyRate, int noOfDays) => dailyRate * noOfDays;
+
+        private int ReadInt(string v)
+        {
+            string noOfDays = ReadUserValue(v);
+            return Int32.Parse(noOfDays);
+        }
+        private double ReadDouble(string v)
+        {
+            var dailyRate = ReadUserValue(v);
+            return double.Parse(dailyRate);
+        }
+
+        private static string ReadUserValue(string v)
+        {
+            Console.Write(v);
+            return Console.ReadLine();
+        }      
+    }
+}
---
+++ methods/methods/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //metoda wcielająca wyrażenie
+            int minusNumbers(int a, int b) => a - b;
+            int plusNumbers(int a, int b) => Add(a, b);
+
+            //próba rzutowania typu
+            double x = 10;
+            float y = 10f;
+           // Add(x, y); // cannot convert doble and float to int
+            int t = 1;
+            int u = 1;
+            Multi(t, u); // nie jawnie rzutuje inta na inny typ
+
+            //krotka (tuple) mała kolekcja wartości. Aby użyc krotki nalezy zainstalować pakiet ValueTuple.
+            int retValue1, retValue2;
+            (retValue1, retValue2) = Devide(11, 2);
+
+            //Testy
+            CalculateSalary.Start();
+
+            ShowResult(retValue1, retValue2);
+            Console.ReadKey();
+        }
+
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        static double Multi(double a, float b)
+        {
+            return a * b;
+        }
+
+        static (int, int) Devide(int a, int b)
+        {
+            var devidedResult = a / b;
+            var remaider = a % b;
+            return (devidedResult, remaider);
+        }
+
+
+        static void ShowResult(int result)
+        {
+            Console.WriteLine($"result: {result}");
+        }
+
+        static void ShowResult(int result1, int result2)
+        {
+            Console.WriteLine($"result: {result1} and {result2}");
+        }
+    }
+}
