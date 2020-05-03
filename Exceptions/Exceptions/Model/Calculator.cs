4bb52bf54d0a4048851fd262bdf6542ca2533dac
dota2 <dota2@poczta.onet.pl>
2020-05-03 23:36:26 +0200

    added throw and finally

--- Exceptions/Exceptions/Model/Calculator.cs
+++ Exceptions/Exceptions/Model/Calculator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Exceptions.Model
 {
@@ -8,18 +9,21 @@ namespace Exceptions.Model
         private static int b;
         private static bool catchError = false;
         private static char CalulateOperand;
-        private static decimal Reslut;
+        private static decimal Result;
+        private static List<decimal> ListOfResluts;
 
         public static void On()
         {
+            InitalizeListOfResluts();
             InputLeftOperand();
             InputCalulateOperand();
             InputRightOperand();
-
+            
             switch (CalulateOperand)
             {
                 case '+':
-                    Reslut = Add();
+                    Result = Add();
+                    ListOfResluts.Add(Result);
                     break;
                 case '-':
                 case '*':
@@ -27,7 +31,7 @@ namespace Exceptions.Model
                     break;
             }
 
-            DisplayResult(Reslut.ToString());
+            DisplayResult(Result.ToString());
         }
 
         public static int Add()
@@ -53,6 +57,7 @@ namespace Exceptions.Model
             }
             catch (Exception Ex) when (catchError == true)
             {
+                //Note: Filtry wyjątków. Obsługa wyjątków tylko gdy catchError = true
                 Console.WriteLine("Filtry wyjątków. Obsługa wyjątków tylko gdy catchError = true");
                 Console.WriteLine(Ex.Message);
             }
@@ -117,17 +122,48 @@ namespace Exceptions.Model
             catch (FormatException fEx)
             {
                 Console.WriteLine($"Input value is not a number. Error message: {fEx.Message}");
-                decimal c = a / b; //b is 0 (zero). 
-                Console.WriteLine("W tym przypadku nie zostanie wykonana obsługa wjątku która jest niżej. Wyjątek zostanie zwrócony do metody która wywołała tę metode." +
-                    " Jeśli nie znajdzię obsługi wyjątku DivideByZeroException, procedura się powtórzy. Jeśli wyjątek nie znajdzie odpowieniej obsługi wyjątku, program zakonczy się sygnalizując nie obsłużony wyjątek. (unhandled exception)");
+                decimal c = a / b; //b is 0 (zero).
+                //Note: a i b są intem, takie działanie wyrzuci wyjątek 'OverflowException'. Opertacje na liczbach zmiennoprzecinkowych nigdy nie zgłoszą wyjątku przepełnienia 'OverflowException',
+                // nawet przy dzieleniu przez 0.0 gdyż .NET posiada specjalną zmienna reprezentującą nieskonczoność. 'double.PositiveInfinity'
+
+                //Note: W tym przypadku nie zostanie wykonana obsługa wjątku która jest niżej 'DivideByZeroException'. Wyjątek zostanie zwrócony do metody która wywołała tę metode.
+                // Jeśli nie znajdzię obsługi wyjątku DivideByZeroException, procedura się powtórzy. Jeśli wyjątek nie znajdzie odpowieniej obsługi wyjątku, program zakonczy się sygnalizując błąd: nie obsłużony wyjątek. (unhandled exception)");
             }
             catch (DivideByZeroException dEx)
             {
                 Console.WriteLine(dEx.Message);
             }
 
-            Console.WriteLine("This instruction will only executed if FormatException occurs or no exception occurs.");
-            Console.WriteLine("W przypadku gdy wystąpi wyjątek i nie bedzie to FormatException nie wyswietli się ten komunikat.");
+            Console.WriteLine("This instruction will only executed if FormatException, DivideByZeroException occurs or no exception occurs.");
+            //Note: W przypadku gdy wystąpi wyjątek i nie bedzie to FormatException lub DivideByZeroException nie wyswietli się ten komunikat.
+        }
+
+        private static void InitalizeListOfResluts()
+        {
+            if(ListOfResluts == null)
+            {
+                ListOfResluts = new List<decimal>();
+            }
+        }
+
+        public static void Off()
+        {
+            try
+            {
+                a = 10;
+                b = 0;
+                Console.Clear();
+                Console.WriteLine($"This instruction throw exception: 10/0 = {a/b}. But before exception will be handled, execute finally content.");
+                //Note: Najpierw wykona się blok finally a potem catch DivideByZeroException w metodzie wyżej.
+            }
+            finally
+            {
+                if (ListOfResluts != null)
+                {
+                    ListOfResluts.Clear();
+                }
+            }
+           
         }
     }
 }
--- Exceptions/Exceptions/Program.cs
+++ Exceptions/Exceptions/Program.cs
@@ -10,6 +10,12 @@ namespace Exceptions
             try
             {
                 //Calculator.Add();
+
+                //Note: Domyślnie wszystkie operacje są unchecked znaczy się nie jest sprawdzany czy int przekroczył maxymalną wartość. Ma to wpływ na wydajność.
+                // checked i unchecked działa dla kodu (operacji arytmetycznych) bezpośrednio w nawiasach {}, nie działa dla kodu który jest w funkcjach, np. Niezadziała gdy w medodzie Add() przekroczy int maxymalną wartość.
+                // Te słowa kluczowe działają wyłącznie dla operacji arytmetycznych liczb całkowitych int, long. Operacji arytmetyczne na liczbach zmiennoprzecinkowych nigdy nie zgłoszą wyjątku przepełnienia 'OverflowException',
+                // nawet przy dzieleniu przez 0.0 gdyż .NET posiada specjalną zmienna reprezentującą nieskonczoność. 'double.PositiveInfinity'
+                // Można zmienić domyśle dziłanie odznaczając checkbosa "Check for arytmetic overflow/underflow" w właściwościach projektu. Properties -> Build -> Advance
                 unchecked
                 {
                     int a = Calculator.Add(int.MaxValue, 1);
@@ -19,29 +25,21 @@ namespace Exceptions
                     int a = Calculator.Add(int.MaxValue, 1);
                 }
 
-                //Note: Domyślnie wszystkie operacje są unchecked znaczy się nie jest sprawdzany czy int przekroczył maxymalną wartość. Ma to wpływ na wydajność.
-                // checked i unchecked działa dla kodu (operacji arytmetycznych) bezpośrednio w nawiasach {}, nie działa dla kodu w funkcjach, przykład wyżej.
-                // Te słowa kluczowe działają wyłącznie dla operacji arytmetycznych liczb całkowitych int long. Operacji arytmetyczne na liczbach zmiennoprzecinkowych nigdy nie zgłoszą wyjątku przepełnienia OverflowException,
-                // nawet przy dzieleniu przez 0.0 gdyż .NET posiada specjalną zmienna reprezentującą nieskonczoność. double.PositiveInfinity;
-                // Można zmienić domyśle dziłanie odznaczając checkbosa "Check for arytmetic overflow/underflow" w właściwościach projektu. Properties -> Build -> Advance
-
                 int overflowInt = unchecked(int.MaxValue + 1);
                 checked
                 {
                     int w = int.MaxValue;
                     //w++; // OverflowException
                 }
-
                 Console.WriteLine(overflowInt); // minumum int 
 
-
-                // Zgłaszanie wyjątków. Throw. Uzywamy gdy chcemy zgłosić jakiś wyjątek np. gdy podamy nieprawidłowy operator arytmetyczny. 1 ? 4
+                //Zgłaszanie wyjątków. Throw. Uzywamy gdy chcemy zgłosić jakiś wyjątek np. gdy podamy nieprawidłowy operator arytmetyczny, np. zamiast operatora mnozenia '*' podamy np. 'x'.
                 Calculator.On();
-
+                Calculator.Off();
             }
             catch (OverflowException oEx)
             {
-                Console.WriteLine("This instruction will excecute when in method Calculator.Add() occurs exception OverflowException (It is call Propagowanie Wyjatków).");
+                Console.WriteLine("This instruction will excecute when exception 'OverflowException' occurs in method Calculator.Add() (It is call Propagowanie Wyjatków).");
                 Console.WriteLine("Propagowanie wyjatkow: wynoszenie wyjątkow, które nie zostały obsłużone w metodze których wystąpiły do metod które je wywołały.");
                 Console.WriteLine("Oprócz wyjątku zostanie także zwrócone sterowanie programu. Następna instrukcja kodu zostanie wykonana w tej klasie.");
                 Console.WriteLine(oEx.Message);
@@ -53,9 +51,13 @@ namespace Exceptions
                     "Try catch sprawdza jakiego wyjatek wystąpił. Exception przechwyci każdy rodzaj wyjatku, gdyż wszystkie wyjątki dziedziczą właśnie po klasie 'Exception'.");
                 Console.WriteLine(ex.Message);
             }
+            finally{
+                //Note: Blok zawsze zostanie wykonany pod koniec bez względu na to czy zostanie zgłoszony wyjątek czy nie.
+                // W przypadku gdy metoda zgłosiła wyjątek i nie został on obsłużony w owej w metodzie wtedy blok finally zostanie najpierw. Przykład - Calculator.Off();
+            }
 
-            //Note: Niektóe wyjątku mogą być spowodowane przez inne wyjątki. Czyli zródło problemu jest gdzie indziej. Możemy to przeanalizować sprawdzając InnerException szukajac zródła problemu. 
-            // Jeśli InnerException = null znaczy, że jestesmy u celu
+            //Note: Niektóre wyjątki mogą być spowodowane przez inne wyjątki. Czyli źródło problemu jest gdzie indziej. Możemy to przeanalizować sprawdzając InnerException szukajac zródła problemu. 
+            // Jeśli InnerException = null oznacza to, że jestesmy u celu
 
             //Tip: Menu Debug -> Windows ->  Exceptions setting -> Pokaze sie ramka w dole ekranu -> rozwijamy Common Language Runtime Exceptions -> wybieramy exception który nas interesuje.
             // W tej chwili jeśli dany exception wystąpi, Debuger przejdzie do kodu który spowodował exception wyswietlając uzyteczną ramke z informacjami o wyjątku.
