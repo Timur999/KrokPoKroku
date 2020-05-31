34ea8129a9b97ea0a90b6e9cc7cfcaebd3b67ccd
dota2 <dota2@poczta.onet.pl>
2020-06-01 00:02:42 +0200

    first part

---
+++ Assemblies/Assemblies/Program.cs
@@ -0,0 +1,20 @@
+﻿
+//Note: .Net standard jest to cześć w spólna bibliotek .net framework i .net core
+
+// Propierties znajduje się plik AssemblyInfo.cs zawiera on atrybuty m.in. o nazwie autora, daty utworzenia projektu  itd. Może również posiadać atrybuty zmieniające działanie programu.
+
+// References znajdują się biblioteki. Podczas kompilacji kodu c# następuje konwersja kodu na biblioteke zwanej także assemblą, której zostanie nadana unikatowa nazwa.
+
+// App.config plik konfiguracynjny określający działanie prrogramu np. nr wersji .net frameworku
+
+// bin i obj są tworzone podczas budowania programu i zawierają wykonywalne pliki programu oraz pewne pliki potrzebne do budowania aplikacji i jej debagowania. (w Solution Explorer-> Show All Files)
+namespace Assemblies
+{
+    class Program
+    {
+        // Metoda Main jest to tzw. 'Punkt wejścia programu".
+        static void Main(string[] args)
+        {
+        }
+    }
+}
