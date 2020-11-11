74af00501cf84217a843cd2869b890105961cd97
dota2 <dota2@poczta.onet.pl>
2020-11-11 22:15:38 +0100

    assemblacje i namespace

---
+++ Chapter 1 Wprowadzenie/Chapter 1 Namespaces/Program.cs
@@ -0,0 +1,47 @@
+﻿/* Przestrzenie nazw powstały dlatego aby zmniejszyć problem z nazewnictwem, konfliktem nazw klas itd.
+ Im wieksza liczba klas tym większy problem z nazewnictwem.
+ Dzięki przestrzeni nazw, klasy mogą mieć taką samą nazwę, podwarunkiem, że istnieją w różnych przestrzeniach.
+ Zaleca się aby w najwyższej hierarchi pliku projektu, używać nazwy projektu jako nazwy przestrzeni nazw (namespace Chapter_1_Namespaces).
+*/
+
+/* Załóżmy, że przestrzeń nazw to Pokój, projekt to Mieszkanie a solucja to Blok. Dyrektywa using to drzwi do pokoju.
+ * W Pokoju mogą znajdować się różne obiekty jak Tapczan Lampa itp. - to nasze klasy.
+ * W Mieszkaniu musi sie znajdować przynajmniej jeden pokój, który posiada metode Main (wejscie do programu).
+ * Może się znajdować wiecej pokoi a także referencje do innych mieszkań (folder References - książka adresowa do innych projektów (bibliotek)).
+ * Chcąc skorzystać z innych klas których nie zdefinowaliśmy w naszym projekcie musimy dodać referencje do tego projektu.
+ * Inaczej mowiąc dodać adres mieszkania do ksiązki adresowej projektu.
+ */
+
+/* W środowisku .Net Framework istnieje mnóstwo takich projetów (inaczej też mówi się na nie assemblace). Każda z nich dostarcza różne funkcjonalności
+ * z których możemy skorzystać w naszych aplikacjach. Istnieje taka główną asemblacja (zestaw ten nosi nazwie mscorlib.dll). Zawiera powszechnie używane
+ * klasy, takie jak System.Console, inne assemblacje zawierający klasy służace do pracy z bazą danych, korzystające z usług webowych, tworzenia interface
+ * graficznego, itd.
+ * Assemblacja może mieć wiele przestrzni nazw (mieszkanie może mieć wiele pokoi)
+ * Uwaga: Ta sama przestrzeń nazw może się zawierać w wielu assemblacjach, np. Klasy i elementy z przestrzeni nazw System zostały faktycznie zaimplementowane
+ * w kilku różnych assemblacjach, miedzy innymi w mscorlib.dll, System.dll, System.Core.dll.
+ */
+
+/* Podczas tworzenia projektu, VS automatycznie doda odpowiednie odwołania do bibliotek w zależności od wybranego szablonu np. Console application.
+ * Jednak w żadnym z projektów nie znajdziemy asemblacji mscorlib.dll. Wynika to z faktu, że ta asemblacja zawiera podstawowe funkconalności czasu wykonywania 
+ * i musi być używana przez wszystkie aplikacje korzystające z platformy .NET Framework.
+ */
+
+// Przestrzenie nazw to poprostu kontenery dla identyfikatorów np. nazwy klas.
+namespace Chapter_1_Namespaces
+{
+    /* Dyrektywa using pozwala korzystac z klas i elementów zawartych w przestrzeni nazw bez wpisywania pełnej nazwy.
+     np. pełna nazwa klasy Console zawartej w przestrzeni nazw System to System.Console.
+     Dyrektywa using dodaje wskazaną przestrzeń nazw do zasięgu (ang. scope).*/
+using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello");
+            Console.Read();
+        }
+    }
+}
+
---
+++ Chapter 1 Wprowadzenie/Chapter 1 Wprowadzenie/Program.cs
@@ -0,0 +1,32 @@
+﻿
+/* Tworząc projekt w technologii .Net możemy wybrać z którego frameworka nasz projekt bedzie korzystać.
+ .Net Core przeznaczony jest do aplikacji przenośnych, które działają na wielu systemach operacyjnych np. Linux. 
+ .Net Framework zawiera inne biblioteki niż .Net Core. Przeznaczona jest do aplikacji uruchamianych na Windowsie.
+ .Net Standard zawiera tylko te biblioteki które występują tylko w .Net Core i .Net Frameworku.
+*/
+// Note: Framework zawiera środowisko uruchomieniowe języka wspólnego CLR oraz biblioteki. Każda nowa wersja dodaje nowe funkcjionalności i zachowuje stare.
+/* 
+    Aplikacje C# składają się z:
+    1. Solucja(Rozwiązanie) jest to plik, który w hierarchi plików aplikacji leży najwyżej. Jest tworzony aby ułatwić prace z projektami.
+Solucja może posiadać jeden projekt lub kilka.
+    2. Project (Chapter 1 Wprowadzenie jest plikiem projektu) Każdy plik projektu zawiera odwołanie przynajmniej do jednego pliku zawierającego kod źródłowy,
+pliku graficznego itp. Całość kodu źródłowego w danym projekcie musi zostać napisana w tym samym języku.
+    3. Properties jest to folder należący do projektu. Zawiera klase AssemblyInfo.cs, jest to plik który umożliwia dodanie specjalnych atrybutów do programu
+takich jak nazwa autora, data utworzenia programu itp. Możliwe jest dodanie takiego atrybutu który zmienia działanie programu. Wykracza to poza ramy książki.
+    4. References (Odwołania) ten folder zawiera odwołania do bibliotek, ktore mogą być używane przez tworzoną aplikacje. 
+Podczas kompilacji kod źródłowy jest przekształcany na bibliotekę. W środowisku .NET Framework biblioteki te nazywane są asemblacjami.
+    4. App.config jest to plik konfiguracyjny, umożliwia dodanie daych konfiguracyjnych takich jak np. wersja środowiska .NET Frameworku używana
+do uruchomienia tej aplikacji. 
+    5. Program.cs plik źródłowy zawiera kod tworzonej aplikacji. (Plik źródłowy to plik od którego komputer zaczyna działanie).
+*/
+namespace Chapter_1_Wprowadzenie
+{
+    class Program
+    {
+        // Ta funkcjia rozpoczyna działanie programu. Jest nazywana punktem wejścia do programu. Metoda musi być static i zaczynać się od Wielkiej litery.
+        static void Main(string[] args)
+        {
+        }
+        // W folderze projektu znajduje się folder bin w tym folderze znajduje się wykonywalna wersja programu.
+    }
+}
