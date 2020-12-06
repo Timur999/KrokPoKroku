642a48f477d818682ff4ef0f20da6f20102be135
dota2 <dota2@poczta.onet.pl>
2020-12-06 17:48:06 +0100

    if else i switch (klucz do programowania :>)

--- Chapter 4 Instrukcje Wyboru/Chapter 4 Instrukcje Wyboru/Program.cs
+++ Chapter 4 Instrukcje Wyboru/Chapter 4 Instrukcje Wyboru/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Chapter_4_Instrukcje_Wyboru
 {
@@ -11,5 +7,72 @@ namespace Chapter_4_Instrukcje_Wyboru
         static void Main(string[] args)
         {
         }
+
+        static void InstrukcjaWarunkowaIfElse()
+        {
+            bool wynik;
+            int zmienna1, zmienna2, zmienna3 = 10;
+            zmienna1 = zmienna2 = zmienna3;
+
+            // Wskazówka: zaleca się aby w instrukcji warunkowej uzywać nawiasy np.
+            wynik = zmienna1 < 11 && zmienna2 < 12 && zmienna3 < 13; // Nie zalecane
+            wynik = (zmienna1 < 11) && (zmienna2 < 12) && (zmienna3 < 13); // Zalecane
+
+            // Do porównywania dat. Klasa DateTime posiada metode statyczną Compare
+            DateTime dt1 = new DateTime(1999, 8, 1);
+            DateTime dt2 = new DateTime(1999, 9, 10);
+            DateTime.Compare(dt1, dt2);
+        }
+
+        static void SkracanieDzialania()
+        {
+            bool warunek1 = false;
+            bool warunek2 = true;
+            // Jeśli instrukcja warunkowa która składa się z 2 warunków i && to gdy pierwszy warunek jest false, drugi warunek nie jest sprawdzany.
+            if (warunek1 && warunek2)
+                Console.WriteLine("Nie sprawdzi drugiego warunku");
+
+            // Jeśli instrukcja warunkowa która składa się z 2 warunków i || to gdy pierwszy warunek jest true, drugi warunek nie jest sprawdzany.
+            if (warunek2 || warunek1)
+                Console.WriteLine("Nie sprawdzi drugiego warunku");
+        }
+
+        static void InstrukcjaSwitch()
+        {
+            /* Zasady które musi spełnić instrukcja switch: 
+             * - Używana jest tylko do porstych typów danych.
+             * - Etykiety case muszą być wyrażeniami stałymi. Wartość case nie może być zwrócona przez np. metode.
+             * - Etykiety case nie mogą być takie same. Inaczej mówiąć wartość casa musi być unikatowa.
+             * - Możliwe jest określenie, że dla różnych wartości sterującej (wartość w switchu) uruchomi się ten sam case. W tym celu należy podać
+             * listę etykiet jedna pod drugą bez żadnych instrukcji pomiędzy.
+             */
+            Napoje napoje = Napoje.Herbata;
+            switch (napoje)
+            {
+                case Napoje.WodaZcytryna:
+                case Napoje.WodaPoOgorkach:
+                    Console.WriteLine("Zdrowa i pic trzeba");
+                    break;
+                case Napoje.Kawa:
+                    Console.WriteLine("Kawa chuj");
+                    throw new Exception("Zbyt duze stezenie kofeiny we krwy. Zawał serca ioioioioio.");
+                case Napoje.Herbata:
+                    Console.WriteLine("Tylko zielona");
+                    break;
+                default:
+                    Console.WriteLine("Jestem woda");
+                    return;
+            }
+
+            //Note: W C# kompilator wymaga aby instrukcje case zakończyc dodając break lub return lub throw.
+        }
+    }
+
+    enum Napoje
+    {
+        Kawa,
+        Herbata,
+        WodaZcytryna,
+        WodaPoOgorkach
     }
 }
